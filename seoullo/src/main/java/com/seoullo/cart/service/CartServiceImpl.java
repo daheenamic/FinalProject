5a7c868e0badcbc5adc88f6a53bbc06c3bc0b2c2
Dahee Jeong <dh910320@naver.com>
2023-04-13 12:42:14 +0900

    Final Project

---
+++ seoullo/src/main/java/com/seoullo/cart/service/CartServiceImpl.java
@@ -0,0 +1,72 @@
+package com.seoullo.cart.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.seoullo.book.mapper.BookMapper;
+import com.seoullo.book.vo.BookVO;
+import com.seoullo.cart.mapper.CartMapper;
+import com.seoullo.cart.vo.CartVO;
+
+import lombok.Setter;
+
+@Service
+@Qualifier("cartServiceImpl")
+public class CartServiceImpl implements CartService {
+	
+	@Setter(onMethod_ = @Autowired)
+	private CartMapper mapper;
+
+	@Override
+	public List<CartVO> list(String id) {
+	    List<CartVO> list = mapper.list(id);
+	    for (CartVO vo : list) {
+	    	CartVO reserve = mapper.checkReserveNum(vo);
+	    	vo.setReserveNum(reserve.getReserveNum());
+	    	vo.setMaxNum(reserve.getMaxNum());
+	    }
+	    return list;
+	}
+
+	// 장바구니에서 주문할 때
+	@Override
+	public List<CartVO> orderList(List<Long> nos) {
+	    List<CartVO> list = new ArrayList<>();
+	    for (Long no : nos) {
+	        List<CartVO> orderList = mapper.orderList(no);
+	        list.addAll(orderList);
+	    }
+	    return list;
+	}
+	
+	@Override
+	public int add(CartVO vo) {
+		return mapper.add(vo);
+	}
+
+	@Override
+	public int update(CartVO vo) {
+		return mapper.update(vo);
+	}
+
+	@Override
+	public int delete(List<Long> nos) {
+	    int result = 0;
+	    for (Long no : nos) {
+	        result += mapper.delete(no);
+	    }
+	    return result;
+	}
+
+	@Override
+	public Long searchCart(CartVO vo) {
+		return mapper.searchCart(vo);
+	}
+
+
+}
